e05dc6c45c927205bea9eff16a9ad5bbeeaf9c1b
yuuCraft <yuucraft1298@proton.me>
2026-01-16 17:11:51 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+window.TOOLS = [
+  {
+    id:"json",
+    name:"JSON Formatter",
+    desc:"JSONを整形・最小化",
+    path:"./tools/json/"
+  },
+  {
+    id:"base64",
+    name:"Base64 Encode / Decode",
+    desc:"Base64変換",
+    path:"./tools/base64/"
+  },
+  {
+    id:"text",
+    name:"Text Tools",
+    desc:"文字列操作",
+    path:"./tools/text/"
+  }
+];
